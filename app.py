687b071e5506232ff2d1fe5e76455a5afa97ba60
christopherivy <cdi.747@gmail.com>
2021-11-16 10:48:25 -0800

    added os for environ

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Tweet
 from forms import UserForm, TweetForm
 from sqlalchemy.exc import IntegrityError
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "abc123"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "Red747")
+# print(app.config["SECRET_KEY"])
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 
